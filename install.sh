2083dd07bd7b809b4e38f706c8b6177132483492
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-12-23 21:17:08 -0200

    refactorings suggested by ShellChecker, quotings, etc.

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 TORRENTING_ROOT_DEFAULT="${HOME}/torrent"
 TORRENTING_ROOT_=${1:-$TORRENTING_ROOT_DEFAULT}
-TORRENTING_ROOT=$(readlink -f $TORRENTING_ROOT_)
+TORRENTING_ROOT=$(readlink -f "$TORRENTING_ROOT_")
 
-if [ ${TORRENTING_ROOT} != ${TORRENTING_ROOT_DEFAULT} ]
+if [ "${TORRENTING_ROOT}" != "${TORRENTING_ROOT_DEFAULT}" ]
 then
-    ln -s -f ${TORRENTING_ROOT} ${TORRENTING_ROOT_DEFAULT}
+    ln -s -f "${TORRENTING_ROOT}" "${TORRENTING_ROOT_DEFAULT}"
 fi
 
 TORRENT_SUBDIRS=('session' 'content-in-progress' 'content-finished' 'metafiles-in-progress' 'metafiles-finished')
@@ -15,8 +15,8 @@ for subdir in "${TORRENT_SUBDIRS[@]}"; do
 done
 
 
-REL_SRC=${BASH_SOURCE[0]}
-CANONICAL_SRC=$(readlink -f $REL_SRC)
+REL_SRC="${BASH_SOURCE[0]}"
+CANONICAL_SRC=$(readlink -f "$REL_SRC")
 DIR="$(cd -P "$(dirname $CANONICAL_SRC)" && pwd)"
 
 ln -s -f "${DIR}/rtorrent.rc"    "${HOME}/.rtorrent.rc"
