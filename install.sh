520065e5807afbc194623a3173a20eaa2203f682
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-12-24 14:34:18 -0200

    made ln commands have the -n option to not-derefence not even directories

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ TORRENTING_ROOT=$(readlink -f "$TORRENTING_ROOT_")
 
 if [ "${TORRENTING_ROOT}" != "${TORRENTING_ROOT_DEFAULT}" ]
 then
-    ln -s -f "${TORRENTING_ROOT}" "${TORRENTING_ROOT_DEFAULT}"
+    ln -s -f -n "${TORRENTING_ROOT}" "${TORRENTING_ROOT_DEFAULT}"
 fi
 
 TORRENT_SUBDIRS=('session' 'content-in-progress' 'content-finished' 'metafiles-in-progress' 'metafiles-finished')
@@ -19,8 +19,8 @@ REL_SRC="${BASH_SOURCE[0]}"
 CANONICAL_SRC=$(readlink -f "$REL_SRC")
 DIR="$(cd -P "$(dirname $CANONICAL_SRC)" && pwd)"
 
-ln -s -f "${DIR}/rtorrent.rc"    "${HOME}/.rtorrent.rc"
+ln -s -f -n "${DIR}/rtorrent.rc"    "${HOME}/.rtorrent.rc"
 
 mkdir -p "${HOME}/bin"
-ln -s -f "${DIR}/magnet.sh"      "${HOME}/bin/magnet.sh"
+ln -s -f -n "${DIR}/magnet.sh"      "${HOME}/bin/magnet.sh"
 
