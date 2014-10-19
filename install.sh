57136520f1d3e981cf116e0fb8c557e4639b4f8e
João Paulo Pizani Flor <joaopizani@gmail.com>
2014-10-19 12:31:44 +0200

    Little revamp in README and install

--- install.sh
+++ install.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env bash
 
-TORRENTING_ROOT_DEFAULT="${HOME}/torrent"
-TORRENTING_ROOT_="${1:-$TORRENTING_ROOT_DEFAULT}"
-TORRENTING_ROOT="$(readlink -f "$TORRENTING_ROOT_")"
+TORR_ROOT_DEFAULT="${HOME}/torrent"
+TORR_ROOT_="${1:-$TORR_ROOT_DEFAULT}"
+TORR_ROOT="$(readlink -f "$TORR_ROOT_")"
 
-if [ "${TORRENTING_ROOT}" != "${TORRENTING_ROOT_DEFAULT}" ]
-then
-    ln -s -f -n "${TORRENTING_ROOT}" "${TORRENTING_ROOT_DEFAULT}"
-fi
+[ "${TORR_ROOT}" != "${TORR_ROOT_DEFAULT}" ] && ln -s -f -n "${TORR_ROOT}" "${TORR_ROOT_DEFAULT}"
 
-TORRENT_SUBDIRS=('session' 'content-in-progress' 'content-finished' 'metafiles-in-progress' 'metafiles-finished')
-for subdir in "${TORRENT_SUBDIRS[@]}"; do
-    mkdir -p "${TORRENTING_ROOT}/${subdir}"
-done
+TORR_SUBDIRS=('session' 'content-in-progress' 'content-finished' 'metafiles-in-progress' 'metafiles-finished')
+for subdir in "${TORR_SUBDIRS[@]}"; do mkdir -p "${TORR_ROOT}/${subdir}"; done
 
 
 DIR="$(cd -P "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd)"
 
 ln -s -f -n "${DIR}/rtorrent.rc"    "${HOME}/.rtorrent.rc"
 
-"${DIR}/magnet-get/install.sh"  "${TORRENTING_ROOT}"
+"${DIR}/magnet-get/install.sh"  "${TORR_ROOT}"
 
